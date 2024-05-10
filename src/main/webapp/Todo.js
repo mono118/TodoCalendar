007f4393149603ba3b9c9be96c05ad8c8a15b377
Saku <sakuto.nns421@gamil.com>
2024-05-10 11:51:29 +0900

    Todoに入力された文を、taskListに追加する処理を追加
    とりあえずtaskNameの部分だけ完成した

--- src/main/webapp/Todo.js
+++ src/main/webapp/Todo.js
@@ -1,13 +1,22 @@
 function taskAdding(){
-    let taskName = document.getElementById("tName");
+    let taskName = document.getElementById("tName").value;
     let taskDate = document.getElementById("tDate");
     let taskTags = document.getElementById("tTag");
     let taskMemo = document.getElementById("tMemo");
     let taskTagList = tagmake(taskTags.value);
     
     const goingListDiv = document.getElementById("ongoingList");
-    let list = "" //追加するタスクを記述していく
-    list += "<li class=\"task\">" + "<input class=\"finished\" type=\"checkbox\">" + taskName;
+    let list = document.createElement("li") //追加するタスクを記述していく
+    list.className = "task";
+    let input = document.createElement("input");
+    input.className = "finished";
+    input.type = "checkbox";
+    list.appendChild(input);
+    const listName = document.createTextNode(taskName);
+    list.appendChild(listName);
+    //リストに追加
+    const goingul = goingListDiv.getElementsByClassName("taskList")[0];
+    goingul.appendChild(list);
 }
 
 function tagmake(Tags){
