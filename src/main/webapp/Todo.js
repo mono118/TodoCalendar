8039debc3e3869108bdfd2627c6f0995f8330cd7
Saku <sakuto.nns421@gmail.com>
2024-05-10 18:51:46 +0900

    checkboxによるリスト間移動の処理を追加

--- src/main/webapp/Todo.js
+++ src/main/webapp/Todo.js
@@ -1,3 +1,6 @@
+const goingList = document.getElementById("ongoingList");
+const completedList = document.getElementById("completedList");
+
 function taskAdding(){
     let taskName = document.getElementById("tName").value;
     let taskDate = document.getElementById("tDate");
@@ -10,11 +13,17 @@ function taskAdding(){
     let input = document.createElement("input");
     input.className = "finish";
     input.type = "checkbox";
+    input.addEventListener("change", function(){ //進行中・終了済みのリスト間移動の処理
+        if(input.checked){
+            completedList.appendChild(goingList.removeChild(list));
+        }else{
+            goingList.appendChild(completedList.removeChild(list));
+        }
+    });
     list.appendChild(input);
     const listName = document.createTextNode(taskName);
     list.appendChild(listName);
     //リストに追加
-    const goingList = document.getElementById("ongoingList");
     goingList.appendChild(list);
 }
 
@@ -24,5 +33,6 @@ function tagmake(Tags){
     return tagList;
 }
 
+//タスクを追加する
 let addButton = document.getElementById("taskAdd");
 addButton.addEventListener("click", taskAdding);
