47b29a7cc780fc458429887504dab8d03895fd33
Saku <sakuto.nns421@gmail.com>
2024-05-16 00:06:45 +0900

    子タスクを追加する処理の追加（途中）

--- src/main/webapp/Todo.js
+++ src/main/webapp/Todo.js
@@ -2,24 +2,34 @@ const goingList = document.getElementById("ongoingList");
 const completedList = document.getElementById("completedList");
 
 function taskAdding(){
-    let taskName = document.getElementById("tName");
-    let taskDate = document.getElementById("tDate");
-    let taskTags = document.getElementById("tTag");
-    let taskMemo = document.getElementById("tMemo");
-    let taskTagList = tagmake(taskTags.value);
+    let todoName = document.getElementById("tName");
+    let todoDate = document.getElementById("tDate");
+    let todoTags = document.getElementById("tTag");
+    let todoMemo = document.getElementById("tMemo");
+    let todoTagList = tagmake(todoTags.value);
     
     let list = document.createElement("li") //追加するタスクを記述していく
     list.className = "task";
     let input = document.createElement("input");
     input.className = "finish";
     input.type = "checkbox";
-    let button = document.createElement("button");
-    button.innerText = "削除";
-    button.addEventListener("click", () => {
+    let deleatButton = document.createElement("button"); //削除ボタン
+    deleatButton.innerText = "削除";
+    deleatButton.addEventListener("click", () => {
         const deletecheck = window.confirm("削除しますか？");
         if(deletecheck){
             list.parentElement.removeChild(list);
         }
+    });
+    let childtaskButton = document.createElement("button"); //子タスク追加ボタン
+    childtaskButton.innerText = "+";
+    childtaskButton.addEventListener("click", () => {
+        let cul = document.createElement("ul");
+        let clist = document.createElement("li");
+        let ctext = document.createTextNode(prompt("タスクの名前を入力"));
+        clist.appendChild(ctext);
+        cul.appendChild(clist);
+        list.appendChild(cul);
     });
     input.addEventListener("change", () => { //進行中・終了済みのリスト間移動の処理
         if(input.checked){
@@ -29,14 +39,15 @@ function taskAdding(){
         }
     });
     list.appendChild(input);
-    list.appendChild(document.createTextNode(taskName.value));
-    taskName.value = ""; //入力ボックスの中身を空に
-    list.appendChild(button);
-    if(taskMemo.value != ""){ //listにメモの追加
+    list.appendChild(document.createTextNode(todoName.value));
+    todoName.value = ""; //入力ボックスの中身を空に
+    list.appendChild(childtaskButton);
+    list.appendChild(deleatButton);
+    if(todoMemo.value != ""){ //listにメモの追加
         let text = document.createElement("p");
         text.innerText = taskMemo.value;
         list.appendChild(text);
-        taskMemo.value = "";
+        todoMemo.value = "";
     }
     //リストに追加
     goingList.appendChild(list);
